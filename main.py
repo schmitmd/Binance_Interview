3fdc8f947b5975e2bae4d2bcffc5eaadf238eb62
Michael D. Schmitt <michael.d.schmitt@colorado.edu>
2020-10-30 22:07:21 -0600

    Add code for handling notional value summation
     - Add --notional flag to specify top number of bids and asks to get per-symbol
       - Skip summation of bids/asks if this flag isn't present
       - Add function to parse this value and compare to valid values for API calls
     - Set code to work on all symbols if no quoteAsset flag is passed (this is bad)
     - Create Dicts with symbol keys to track bids/asks
       - Add up the top bids and asks by symbol and print them
     - TODO: Refactor. main() is getting pretty big and should be split out

--- main.py
+++ main.py
@@ -36,11 +36,18 @@ parser.add_argument(
     "A quote asset type to filter symbols by in the Binance SPOT API (BTC,USDT,etc)",
     required=False,
 )
+parser.add_argument(
+    "-n",
+    "--notional",
+    type=int,
+    help="Number of top bids and asks to get notional values for by symbol",
+    required=False,
+)
 args = parser.parse_args()
 
 
 def make_request(url):
-    """  Hit a URL and return a requests object iff the return code was 200 OK """
+    """  Hit a URL and return a requests object if the return code was 200 OK """
     # TODO: Handle all error codes returned by Binance as documented in
     # https://github.com/binance-exchange/binance-official-api-docs/blob/master/errors.md
     try:
@@ -68,10 +75,19 @@ def get_response_as_json(request_obj):
 
 def get_kline(api_url, symbol, interval="1d"):
     """ Return List of klines for a passed symbol and interval (default to 24h) """
-    # FIXME: I don't like swapping between List and JSON here, can we stick to JSON?
     kline = make_request(api_url + "klines" + "?symbol=" + symbol +
                          "&interval=" + interval)
     return list(kline.json())
+
+
+def get_order_book_request_limit(limit):
+    """ Return a valid request limit parameter (int) for the order book API endpoint """
+    valid_limits = [5, 10, 20, 50, 100, 500, 1000, 5000]
+    for value in valid_limits:
+        if limit <= value:
+            return value
+    sys.exit("The maximum number of top bids/asks you can request is " +
+             str(valid_limits[-1]))
 
 
 def sort_klines_by_volume(kline_list_obj):
@@ -84,9 +100,19 @@ def find_symbols_by_quote_asset(exchange_json_obj, quote_asset_type=None):
     """ Filter symbols by quoteAsset, return List (all symbols if not specified) """
     symbol_list = []
     for item in exchange_json_obj.get("symbols"):
-        if quote_asset_type in item.get("quoteAsset"):
+        if quote_asset_type is None:
+            symbol_list += [item.get("symbol")]
+        elif quote_asset_type in item.get("quoteAsset"):
             symbol_list += [item.get("symbol")]
     return symbol_list
+
+
+# TODO: set allowed numbers for limit option
+def get_order_book(api_url, symbol, limit):
+    """ Get Order Book for a symbol """
+    request = make_request(api_url + "depth" + "?symbol=" + symbol +
+                           "&limit=" + str(limit))
+    return request.json()
 
 
 def main():
@@ -109,7 +135,7 @@ def main():
     # klines for each symbol)
     symbol_dict = {}
     for item in symbol_list:
-        symbol_dict.__setitem__(item, [])
+        symbol_dict[item] = ""
 
     # ThreadPool the API calls for getting klines.
     # TODO: Make this ThreadPooling a function, maybe don't hard-code workers number?
@@ -117,10 +143,7 @@ def main():
         futures = []  # Empty List to track Future objects
         for key in symbol_dict:
             futures.append(
-                executor.submit(get_kline,
-                                api_url=api_url,
-                                symbol=key,
-                                interval="1d"))
+                executor.submit(get_kline, api_url=api_url, symbol=key))
         for future in concurrent.futures.as_completed(futures):
             # Populate the dictionary of symbols (keys) with the top value returned for the klines over the last 24h for that symbol
             for key in symbol_dict:
@@ -135,6 +158,62 @@ def main():
     print("Top 5 symbols by Volume over the last 24h")
     for item in sorted_symbols[0:5]:
         print(item[0])
+
+    # Go do the notional value stuff if requested, otherwise we're done.
+    if args.notional is None:
+        sys.exit(0)
+    else:
+        limit = get_order_book_request_limit(args.notional)
+
+        # Define Dicts
+        notional_dict = {}
+        bids_dict = {}
+        asks_dict = {}
+
+        for item in sorted_symbols[0:5]:
+            # Add those symbols to a new Dict to get the Order Book JSON for each.
+            # FIXME: This one is a wasted Dict, should use a List
+            notional_dict[item[0]] = ""
+            bids_dict[item[0]] = ""
+            asks_dict[item[0]] = ""
+
+    # ThreadPool the API calls for getting order books (see ThreadPool todo above).
+    with concurrent.futures.ThreadPoolExecutor(
+            max_workers=len(notional_dict)) as executor:
+        futures = []
+        for key in notional_dict:
+            futures.append(
+                executor.submit(get_order_book,
+                                api_url=api_url,
+                                symbol=key,
+                                limit=limit))
+        for future in concurrent.futures.as_completed(futures):
+            # Populate the dictionary of symbols (keys) with the Order Book value returned
+            for key in notional_dict:
+                bids_dict[key] = future.result().get("bids")
+                asks_dict[key] = future.result().get("asks")
+            # Trim to requested amount
+            del bids_dict[key][args.notional:]
+            del asks_dict[key][args.notional:]
+
+    # Loop through the notional Dict's keys
+    for item in notional_dict:
+        bids_total = float(0)
+        asks_total = float(0)
+
+        # Loop through the bids_dict's List of Lists, summing up price*quantity (notional value)
+        for i in bids_dict[item]:
+            bids_total += (float(i[0]) * float(i[1]))
+        print(item, "total notional value of top", len(bids_dict[item]),
+              "bids:", bids_total)
+
+        # Loop through the asks_dict's List of Lists, summing up price*quantity (notional value)
+        for i in asks_dict[item]:
+            asks_total += (float(i[0]) * float(i[1]))
+        print(item, "total notional value of top", len(asks_dict[item]),
+              "asks:", asks_total)
+        # NOTE: Is this desired too?
+        #print(item, "total notional value of both:", (bids_total + asks_total))
 
 
 # Execute main function
